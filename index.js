64a29f6d94f983ba6e740414ba28858e1b197933
Madhav Sharma <madhav70sharma@gmail.com>
2025-07-12 02:15:51 +0530

    i

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         message: formData.get('message')
                     };
                     try {
-                        const response = await fetch('http://localhost:3001/api/contact', {
+                        const response = await fetch('https://portfolio-website-ashen-beta-44.vercel.app', {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
